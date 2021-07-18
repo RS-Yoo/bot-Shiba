d970dc0029f0ccd922b6e65b43617eef25cd7e54
NeinShibes <76112694+NeinShibes@users.noreply.github.com>
2021-07-18 19:38:25 +0900

    Initial commit

---
+++ src/main/java/bot/botShiba/BotShibaApplication.java
@@ -0,0 +1,13 @@
+package bot.botShiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotShibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotShibaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bot/botShiba/BotShibaApplicationTests.java
@@ -0,0 +1,13 @@
+package bot.botShiba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotShibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
