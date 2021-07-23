8ddbe4e52c6983af85ba3a44bb8f7c6964e9c353
NeinShibes <76112694+NeinShibes@users.noreply.github.com>
2021-07-23 16:36:02 +0900

    Implemented Twitter Oauth1

--- src/main/java/bot/botShiba/controller/UserController.java
+++ src/main/java/bot/botShiba/controller/UserController.java
@@ -1,15 +1,24 @@
 package bot.botShiba.controller;
 
-import bot.botShiba.model.User;
 import bot.botShiba.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class UserController {
 
     private final UserService userService;
+
 
     @Autowired
     public UserController(UserService userService) {
@@ -18,11 +27,43 @@ public class UserController {
 
     @GetMapping("/joinForm")
     public String create() {
-        User user = new User();
-        user.setUsername("test");
-        user.setPassword("1234");
-        user.setRole("ROLE_USER");
-        userService.join(user);
+        userService.join();
         return "redirect:/index";
     }
+
+    @GetMapping("/twitterLogin")
+    public void twitterLogin(HttpServletRequest request, HttpServletResponse response) throws TwitterException, IOException {
+        Twitter twitter = new TwitterFactory().getInstance(); // 트위터로 접근
+
+        RequestToken requestToken = twitter.getOAuthRequestToken(); //성공 시 requestToken에 해당 정보 겨짐
+        request.getServletContext().setAttribute("requestToken", requestToken);
+
+
+        String authURL = requestToken.getAuthenticationURL();
+
+        response.sendRedirect(authURL);
+    }
+
+    @GetMapping("/twitterAuth")
+    public String twitterOauth(HttpServletRequest request,
+                               @RequestParam("oauth_token") String oauth_token,
+                               @RequestParam("oauth_verifier") String oauth_verifier) throws TwitterException {
+
+        Twitter twitter = new TwitterFactory().getInstance();
+        AccessToken accessToken;
+
+        RequestToken requestToken = (RequestToken) request.getServletContext().getAttribute("requestToken");
+        request.getServletContext().removeAttribute("requestToken");
+
+
+        accessToken = twitter.getOAuthAccessToken(requestToken, oauth_verifier);
+        twitter.setOAuthAccessToken(accessToken);
+
+        System.out.println(accessToken.getUserId());    //트위터의 사용자 아이디
+        System.out.println(accessToken.getScreenName());
+
+        return "/user";
+    }
+
+
 }
--- src/main/java/bot/botShiba/service/UserService.java
+++ src/main/java/bot/botShiba/service/UserService.java
@@ -5,7 +5,6 @@ import bot.botShiba.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 //@Transactional
@@ -20,12 +19,18 @@ public class UserService {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
-    public int join(User user) {
+    public int join() {
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("1234");
+        user.setRole("ROLE_USER");
         String rawPwd = user.getPassword();
         String encPwd = bCryptPasswordEncoder.encode(rawPwd);
         user.setPassword(encPwd);
         userRepository.save(user);
         return user.getId();
     }
+
+
 
 }
